81dac384c0642396a3d2f81e1b6eea4a62df1ea2
camargo <gui.ferreira.camargo@gmail.com>
2024-01-13 20:02:28 -0300

    Adicionando variáveis e README.md

--- src/App.jsx
+++ src/App.jsx
@@ -15,40 +15,45 @@ function App() {
   const calculaIMC = () => {
     let alturaFloat = parseFloat(altura);
     let pesoFloat = parseFloat(peso);
-    let alturaAoQuadrado = alturaFloat * alturaFloat;
-    let imcCalculo = pesoFloat / alturaAoQuadrado;
-    let imcFinal = parseFloat(imcCalculo.toFixed(2));
-  
-    const magrezaLine = document.querySelector("#magreza");
-    const normal = document.querySelector("#normal");
-    const sobrepeso = document.querySelector("#sobrepeso");
-    const obesidade = document.querySelector("#obesidade");
-    const grave = document.querySelector("#grave");
-    magrezaLine.classList.remove('active');
-    normal.classList.remove("active");
-    sobrepeso.classList.remove("active");
-    obesidade.classList.remove("active");
-    grave.classList.remove("active");
 
-    if (imcFinal < 18.50) {
-      magrezaLine.classList.add('active');
-      return setIMC(imcFinal);
-    } 
-    if (imcFinal >= 18.50 && imcFinal < 25.0) {
-      normal.classList.add('active');
-      return setIMC(imcFinal);
-    }
-    if (imcFinal >= 25.0 && imcFinal < 30.0) {
-      sobrepeso.classList.add('active');
-      return setIMC(imcFinal);
-    }
-    if (imcFinal >= 30.0 && imcFinal < 40.0) {
-      obesidade.classList.add('active');
-      return setIMC(imcFinal);
-    } 
-    else {
-      grave.classList.add('active');
-      return setIMC(imcFinal);
+    if (alturaFloat > 0 && pesoFloat > 0) {
+      let alturaAoQuadrado = alturaFloat * alturaFloat;
+      let imcCalculo = pesoFloat / alturaAoQuadrado;
+      let imcFinal = parseFloat(imcCalculo.toFixed(2));
+      const magrezaLine = document.querySelector("#magreza");
+      const normal = document.querySelector("#normal");
+      const sobrepeso = document.querySelector("#sobrepeso");
+      const obesidade = document.querySelector("#obesidade");
+      const grave = document.querySelector("#grave");
+      magrezaLine.classList.remove('active');
+      normal.classList.remove("active");
+      sobrepeso.classList.remove("active");
+      obesidade.classList.remove("active");
+      grave.classList.remove("active");
+
+      if (imcFinal < 18.50) {
+        magrezaLine.classList.add('active');
+        return setIMC(imcFinal);
+      } 
+      if (imcFinal >= 18.50 && imcFinal < 25.0) {
+        normal.classList.add('active');
+        return setIMC(imcFinal);
+      }
+      if (imcFinal >= 25.0 && imcFinal < 30.0) {
+        sobrepeso.classList.add('active');
+        return setIMC(imcFinal);
+      }
+      if (imcFinal >= 30.0 && imcFinal < 40.0) {
+        obesidade.classList.add('active');
+        return setIMC(imcFinal);
+      } 
+      else {
+        grave.classList.add('active');
+        return setIMC(imcFinal);
+      }
+    } else {
+      alert("Altura e Peso devem ter valores acima de 0");
+      location.assign(location.href);
     }
   }
 
